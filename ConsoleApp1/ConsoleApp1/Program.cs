b59a085b5393f4e6ed5c1e6030dbbb272800f035
shibuantonyc <30976419+shibuantonyc@users.noreply.github.com>
2019-05-28 22:20:33 +0400

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //code was edit in git hub
+            //code was edit in git hub again
         }
     }
 }
